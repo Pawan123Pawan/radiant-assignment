039a40cd3dcfb47ae042edc7f78174309e76021f
Pawan123Pawan <pawanmaurya4277@gmail.com>
2024-03-01 05:27:58 +0530

    pushcode

---
+++ src/components/data.js
@@ -0,0 +1,3 @@
+export const data =[
+    {title:"WixPro 72-Inch Responsive Website Builder- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)"}
+]
